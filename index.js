1675bf41ac0df69c92a32994fea908769b879f24
Dan Koskei <qimtaid@gmail.com>
2024-03-21 06:59:43 +0300

    Complete

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function sayHi(name{
+    console.log("Hello "+ name)
+}
+//sayHi('Ash')
+
+const hiRose = function(){
+    console.log('Hi Rose')
+}
+//hiRose()
+
+
+
+const doMath = function(number, number2){
+    return number *number2
+}
+
+doMath(5,5)
